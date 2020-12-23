900529190666174109a9070b6508e2eee8fc0884
SohZongXian <74035273+SohZongXian@users.noreply.github.com>
2020-12-23 16:17:58 +0800

    [Update] Remove suites

--- web-scrape-framework/notebooks/scraper_script/config/config.py
+++ web-scrape-framework/notebooks/scraper_script/config/config.py
@@ -1,7 +1,7 @@
 config = {
     "web": {
         "mode": "windows",
-        "base_url": "http://211.25.204.108:30002/",
+        "base_url": "http://211.25.204.108:30002",
         "timeout": 10, 
         "user_name": "zong.xian.soh@adv-fusionex.com", # fill in username
         "password": "Password123" # fill in password
